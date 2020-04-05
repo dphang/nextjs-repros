574aa7c5320c27c36d8757e1b78e6d2c8546555a
Daniel Phang <dphang12@gmail.com>
2020-04-05 03:44:24 -0700

    Clean up code, fix config.yml

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,6 @@
-const percyHealthCheck = require('@percy/cypress/task');
 const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
 
 module.exports = on => {
   on("file:preprocessor", cypressTypeScriptPreprocessor);
-  on("task", percyHealthCheck);
   on('task', require('@cypress/code-coverage/task'))
 };
