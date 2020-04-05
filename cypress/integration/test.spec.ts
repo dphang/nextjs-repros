a6bbd99c4900a211699636d0ff4ca452ee55db77
Daniel Phang <dphang12@gmail.com>
2020-04-05 04:20:19 -0700

    Add test

--- cypress/integration/test.spec.ts
+++ cypress/integration/test.spec.ts
@@ -1,8 +1,20 @@
 describe("Test page", function() {
-  it("should be able to ping an API", function() {
+  beforeEach(function () {
     cy.server();
     cy.route("POST", "https://www.mocky.io/v2/5185415ba171ea3a00704eed?mocky-delay=5000ms").as("sample");
     cy.visit("/");
+  });
+
+  it("should be able to ping an API", function() {
+    cy.get("[data-cy=button]").click();
+    cy.wait("@sample");
+    cy.get("@sample").should("have.property", "status", 200);
+  });
+
+  it("should be able to ping an API two times", function() {
+    cy.get("[data-cy=button]").click();
+    cy.wait("@sample");
+    cy.get("@sample").should("have.property", "status", 200);
 
     cy.get("[data-cy=button]").click();
     cy.wait("@sample");
