fed153e1ba6149e602b1a096f6762c7b089521a6
Daniel Phang <dphang12@gmail.com>
2020-08-15 16:36:28 -0700

    minor updates

--- perf/perf-test.js
+++ perf/perf-test.js
@@ -5,19 +5,10 @@ import { Rate } from "k6/metrics";
 let failureRate = new Rate("check_failure_rate");
 
 export let options = {
-    stages: [
-        // Linearly ramp up from 1 to 50 VUs during first minute
-        { target: 50, duration: "1m" },
-        // Hold at 50 VUs for the next 3 minutes and 30 seconds
-        { target: 50, duration: "3m30s" },
-        // // Linearly ramp down from 50 to 0 50 VUs over the last 30 seconds
-        { target: 0, duration: "30s" },
-        // Total execution time will be ~5 minutes
-    ],
     thresholds: {
         // Check HTTP request duration is less than the following thresholds
         // Low thresholds since initial page is served by CloudFront cache
-        http_req_duration: ["med<50, p(90)<100, p(95)<200"],
+        http_req_duration: ["med<500"],
         // Thresholds based on the custom metric we defined and use to track application failures
         check_failure_rate: [
             // Global failure rate should be less than 1%
