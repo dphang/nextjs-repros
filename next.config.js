83216661b1931fa0067d8e296c6bb95002c4aa47
Daniel Phang <dphang12@gmail.com>
2020-08-28 08:58:50 -0700

    Subpaths

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   target: 'serverless',
-  trailingSlash: true
+  trailingSlash: false
 }
--- pages/a/b.tsx
+++ pages/a/b.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Index(): JSX.Element {
+export default function b(): JSX.Element {
     return (
         <React.Fragment>
             Hello
